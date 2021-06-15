7e485defd04f9c9fca12bb70da4bdf8568dbf551
Drozdov Artur <artur.drozdov11@gmail.com>
2021-06-15 13:30:23 +0300

    refactor

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-
-
-const renderCard = function(item){
+// 
+// ===================================render card======================================
+const renderCard = function (item) {
 
   const itemCard = document.createElement('div');
 
@@ -28,169 +28,153 @@ const renderCard = function(item){
         </div>
     </div>`;
 
-    
-      itemCard.onclick = function (e) {
-        showModal(item);
-        console.log(item);
-      }
+  itemCard.onclick = function (e) {
 
-    return itemCard;
+    e.target.className == 'addButton' ? addToCart(item) : showModal(item);
 
+    console.dir(e.target)
 
+  }
+  return itemCard;
 }
-
-const renderCards = function(arrItems){
+// 
+// ===================================render cards======================================
+const renderCards = function (arrItems) {
 
   arrItems.forEach(item => {
 
     document.querySelector('.prod__cards').appendChild(renderCard(item));
-   })
+  })
 };
 renderCards(items);
 
-// items.forEach(element => {
-
-  
-
-//   const itemCard = document.createElement('div');
-
-//   itemCard.className = `card`;
-
-//   itemCard.id = `card-${element.id}`;
-
-//   itemCard.innerHTML = `
-//     <div class='topCard__inner'>
-//         <button type='submit' class='heard-button'><img src='img/icons/like_empty.svg' class='icon-heart' alt=''></button>
-//         <div class='topCard-image'><img class='item-image' src='img/${element.imgUrl}' alt=''></div>
-//         <h1 class='product-name'>${element.name}</h1>
-//         <div class='quantity'><b>${element.orderInfo.inStock}</b> left in stock </div>
-//         <div class='price'>Price: <b>${element.price}</b> $</div>
-//         <button class='addButton'>Add to cart</button>
-//     </div>
-//     <div class='botCard__inner'>
-//         <div class='botCard__inner-left'>
-//             <span>${element.orderInfo.reviews}% Positive reviews</span>
-//             <span>Above avarage</span>
-//         </div>
-//         <div class='botCard__inner-right'>
-//             <span>1234</span>
-//             <span>Orders</span>
-//         </div>
-//     </div>`;
-
-//   document.querySelector('.prod__cards').appendChild(itemCard);
-
-//   itemCard.onclick = function () {
-//     showModal(element);
-//   }
 
 
-// });
-
-
-
+// 
+// ===================================render cards======================================
 const arrOfAllColors = [];
 items.map(element => element.color).forEach(item => arrOfAllColors.push(...item));
 const arrOfColors = arrOfAllColors.filter((item, pos, arr) => arr.indexOf(item) == pos);
 
-const arrOfOS=items.map(element=>element.os).filter((item, pos, arr) => arr.indexOf(item) == pos);
+const arrOfOS = items.map(element => element.os).filter((item, pos, arr) => arr.indexOf(item) == pos);
 
-const arrOfDisplays=items.map(element=>element.display).filter((item, pos, arr) => arr.indexOf(item) == pos).sort((a,b)=>a-b);
+const arrOfDisplays = items.map(element => element.display).filter((item, pos, arr) => arr.indexOf(item) == pos).sort((a, b) => a - b);
 console.log(arrOfDisplays);
 
-const arrOfMemory=items.map(element=>element.storage).filter((item, pos, arr) => arr.indexOf(item) == pos).sort((a,b)=>a-b);
+const arrOfMemory = items.map(element => element.storage).filter((item, pos, arr) => arr.indexOf(item) == pos).sort((a, b) => a - b);
 
 console.log(arrOfMemory);
 
-// const accordionColors = document.querySelector('.panel-color__inner');
-{/* <input type="checkbox" id="check2"><label for="check2">Ipsum</label> */}
-
-// function renderFilter(divClass, arrData){
-//   let accordionClass = document.querySelector(divClass);
-
-//   arrData.forEach(item =>{
-
-//     const label = document.createElement('label');
-//     label.innerText= item;
-
-//     const checkbox = document.createElement('input');
-//     checkbox.type = 'checkbox';
-//     label.appendChild(checkbox);
-//     accordionClass.appendChild(label);
-//   })
-
-// }
-
-function renderFilter(divClass, arrData){
-
+// 
+// ===================================render filter======================================
+function renderFilter(divClass, arrData) {
   let accordionClass = document.querySelector(divClass);
-  arrData.forEach(item =>{
 
-    const innerDiv=document.createElement('div');
-    innerDiv.className=`panel__inner-div ${item}-div`;
-
+  arrData.forEach(item => {
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
-    checkbox.id=`checkbox${item}`;
+    checkbox.id = `checkbox${item}`;
+    checkbox.name = item;
+
     const label = document.createElement('label');
-    label.innerText= item;
-    label.setAttribute('for',`checkbox${item}`);
-    innerDiv.append(checkbox,label);
-    accordionClass.append(innerDiv);
+    label.innerText = item;
+    label.setAttribute('for', `checkbox${item}`);
+
+    accordionClass.append(checkbox, label);
+
   })
 }
-
 
 renderFilter('.panel-color__inner', arrOfColors);
 renderFilter('.panel-memory__inner', arrOfMemory);
 renderFilter('.panel-os__inner', arrOfOS);
 renderFilter('.panel-display__inner', arrOfDisplays);
 
-// arrOfColors.forEach(item => {
-
-//   const labelColor = document.createElement('label');
-//   labelColor.innerText= item;
-
-//   const checkboxColor = document.createElement('input');
-//   checkboxColor.type = 'checkbox';
-//   labelColor.appendChild(checkboxColor);
-//   accordionColors.appendChild(labelColor);
+// 
+// ===================================filter logic======================================
 
 
-// })
-// let accordionMenuCheckboxes=document.querySelectorAll('.panel-color__inner label');
-// accordionMenuCheckboxes.forEach(box=>{
-//   box.onclick=e=>{
-//   console.log(e.currentTarget)
-//   e.stopPropagation();
-// };
+const objOfFilters = {};
+let arrOfFilteredItems = [];
+let accordionMenuCheckboxes = document.querySelectorAll('.panel__inner input');
+accordionMenuCheckboxes.forEach(checkbox => {
+  checkbox.onclick =
 
-// })
+    function createObjWithFilters(e) {
+      let val = e.currentTarget.name;
+      let key = e.currentTarget.closest('.panel').previousElementSibling.innerText;
+      console.log(val, key)
+      console.log(arrOfFilteredItems)
+      if (e.currentTarget.name == val) {
+        document.querySelector('.prod__cards').innerHTML = '';
+        arrOfFilteredItems.forEach(item => {
+          arrOfFilteredItems.forEach(item => {
 
-let accordionMenuLabels=document.querySelectorAll('.panel-color__inner label');
-let accordionMenuCheckboxes=document.querySelectorAll('.panel-color__inner input');
+            if (item[key.toLowerCase()] == e.currentTarget.name) {
+              arrOfFilteredItems.splice(arrOfFilteredItems.indexOf(item), 1);
 
-accordionMenuLabels.forEach(box=>{
-  box.addEventListener('click',foo);
-  
-  function foo(e){
-  console.dir(e.currentTarget)
-  e.stopPropagation();
-};
+            }
+            document.querySelector('.prod__cards').innerHTML = '';
+
+            renderCards(arrOfFilteredItems.filter((item, pos, arr) => arr.indexOf(item) == pos));
+            return;
+          })
+          if (item[key.toLowerCase()].includes(e.currentTarget.name)) {
+            for (let i = 0; i < 5; i++) {
+              arrOfFilteredItems.forEach(item => arrOfFilteredItems.splice(arrOfFilteredItems.indexOf(item), 1))
+            }
+
+            document.querySelector('.prod__cards').innerHTML = '';
+            renderCards(arrOfFilteredItems);
+            return;
+          }
+        })
+      }
+      if (arrOfFilteredItems.length == 0) {
+        renderCards(items);
+      }
+
+      if (!Object.keys(objOfFilters).includes(key))
+        objOfFilters[key] = [val];
+      else {
+        if (objOfFilters[key].includes(val)) {
+          // console.log(objOfFilters[key].indexOf(val))
+          objOfFilters[key].splice(objOfFilters[key].indexOf(val), 1);
+          return;
+        }
+        Object.keys(objOfFilters).includes(key) ? objOfFilters[key].push(val) : objOfFilters.key = val;
+      }
+      console.log(objOfFilters);
+
+
+      renderFilteredArr();
+    }
+
 });
-accordionMenuCheckboxes.forEach(box=>{
-  box.addEventListener('click',foo);
-  box.addEventListener('click',foo);
-  
-  function foo(e){
-  console.dir(e.currentTarget)
-  e.stopPropagation();
-};
-
-});
 
 
 
+function renderFilteredArr() {
+
+  document.querySelector('.prod__cards').innerHTML = '';
+
+  for (key in objOfFilters) {
+    items.forEach(item => {
+
+      objOfFilters[key].forEach(val => {
+
+        if (item[key.toLowerCase()].includes(val)) {
+          arrOfFilteredItems.push(item)
+        }
+
+      })
+
+    })
+  }
+  renderCards(arrOfFilteredItems.filter((item, pos, arr) => arr.indexOf(item) == pos));
+}
+
+//
 //=================================================================modal=======================
 const modal = document.createElement('div');
 modal.className = 'modal';
@@ -232,8 +216,6 @@ const showModal = (elemData) => {
   </div>
   </div>
   `
-
-
 }
 
 //=================================================================accordion=======================
@@ -251,12 +233,10 @@ for (let i = 0; i < accordion.length; i++) {
 
   });
 }
-//==================================================================================================
+
 let filterButton = document.querySelector('.filter-button');
 
 filterButton.onclick = function () {
-
-
 
   let sectionFilter = document.querySelector('.main__section-filter'),
     prodCards = document.querySelector('.prod__cards');
@@ -267,16 +247,85 @@ filterButton.onclick = function () {
 }
 
 
-$( function() {
-  $( "#slider-range" ).slider({
+//=================================================================cart=======================
+
+const cart = {
+  items: [],
+  totalItems: 0,
+  totalPrice: 0,
+}
+
+function addToCart(item) {
+  const itemInCart = cart.items.find(itemInCart => itemInCart.id === item.id);
+
+
+  if (!itemInCart) {
+    cart.items.push({
+      id: item.id,
+      data: item,
+      amount: 1,
+    })
+  } else {
+    itemInCart.amount++;
+  }
+  const totalParams = cart.items.reduce((acum, item) => {
+    return {
+      totalItems: acum.totalItems + item.amount,
+      totalPrice: acum.totalPrice + (item.amount * item.data.price),
+    }
+  }, {
+    totalItems: 0,
+    totalPrice: 0,
+  })
+
+  Object.assign(cart, totalParams);
+  renderCart();
+}
+// console.log(cart);
+
+
+const cartInner = document.querySelector('.cart');
+if (cart.items.length == 0) {
+  // console.log(cart.items.length)
+  cartInner.innerHTML = '<span>Card is empty</span>';
+}
+
+function renderCart() {
+
+  const cartInner = document.querySelector('.cart');
+
+  cartInner.innerHTML = '';
+  cart.items.forEach(item => {
+
+    cartInner.innerHTML += `
+    <img src='img/${item.data.imgUrl}'>
+    <span>${item.data.name}</span>
+    `
+
+  })
+
+}
+
+const basket = document.querySelector('.basket img');
+basket.onclick = function () {
+
+
+  const cart__inner = document.querySelector('.cart');
+  cart__inner.classList.toggle('cart__toggle');
+}
+
+$(function () {
+  $("#slider-range").slider({
     range: true,
     min: 0,
     max: 500,
-    values: [ 75, 300 ],
-    slide: function( event, ui ) {
-      $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+    values: [150, 300],
+    slide: function (event, ui) {
+      $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
     }
+
   });
-  $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-    " - $" + $( "#slider-range" ).slider( "values", 1 ) );
-} );
+
+  $("#amount").val("$" + $("#slider-range").slider("values", 0) +
+    " - $" + $("#slider-range").slider("values", 1));
+});
